446031a2ebd63687e6a3dbd63e6c132bc987936c
dangkicchi <toshiro.fujisaki@gmail.com>
2021-06-01 10:59:04 +0900

    Vue.js Udemy学習ソースコーディング新規追加

---
+++ hello/js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+	el: '#app',
+  data: {
+  	message: 'Hello World!',
+    url: 'https://jp.vuejs.org/',
+    toggle: true,
+    languages: [
+    	'Javascript',
+      'Ruby',
+      'Python'
+    ]
+  },
+  methods: {
+  	clickHandler: function() {
+    	this.message = 'Clicked!'
+    }
+  }
+})
