435f5bb47079ac18715efcc294475a2a09928cda
dangkicchi <toshiro.fujisaki@gmail.com>
2021-06-01 11:10:12 +0900

    BKのZIPを展開、整理_01

---
+++ _BK/hello_01_ディレクティブ例/js/main.js
@@ -0,0 +1,48 @@
+let app = new Vue({
+	// options
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+
+  // data: {
+  //   now: ''
+  // },
+  // methods: {
+  //   onclick: function() {
+  //     //alert('onclick!');
+  //     this.now = new Date().toLocaleString();
+  //   }
+  // }
+
+  // data: {
+  // 	user: {
+  //     firstName: 'Taro',
+  //     lastName: 'Yamada',
+  //     age: 28
+  //   }
+  // }
+
+  // data: {
+  //   colors: ['Red', 'Green', 'Blue']
+  // }
+
+  // data: {
+  // 	toggle: true
+  // }
+
+  // data: {
+  //   message: 'Hello Vue.js!'
+  // }
+
+  // data: {
+  // 	message: 'Hello Vue.js!',
+  //   count: 0,
+  //   user: {
+	//     	lastName: 'Yamada',
+  // 	    firstName: 'Taro',
+  //   	  prefecture: 'Tokyo'
+  //     },
+  //     colors: ['Red', 'Green', 'Blue']
+  //   }
+})
---
+++ _BK/hello_02_コンポーネント例/js/main.js
@@ -0,0 +1,7 @@
+Vue.component('hello-component', {
+  template: '<p>Hello!</p>'
+})
+
+let app = new Vue({
+  el: '#app'
+})
